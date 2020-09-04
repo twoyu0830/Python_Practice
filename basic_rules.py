437ca44f3613896cdad6f07bc5a34234ed1d76ce
twoyu0830 <yygg0830@gmail.com>
2020-09-04 23:27:25 +0900

    Basic Rules

--- basic_rules.py
+++ basic_rules.py
@@ -3,3 +3,9 @@
 5678
 """
 print("안녕") #주석은 '#'표시뿐만 아니라 """을 내용 아래, 윗줄에 적어서도 사용할 수 있다.
+
+for i in range(5): 
+    print(i) #들여쓰기 is basic.
+
+age = int(input("Enter your age.")) #input함수는 기본적으로 문자열을 반환한다.
+print(age + 5) #이런 연산이 되게 하려면 int로 casting해야 한다.
