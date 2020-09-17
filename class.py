f133a1385aa574905581a8b6ffcf3844916b606d
Lee Yungyu <70426825+twoyu0830@users.noreply.github.com>
2020-09-17 21:44:11 +0900

    Update class.py

--- class.py
+++ class.py
@@ -1,6 +1,6 @@
 #클래스가 만들어진 이유: 다양한 것을 담고 싶어서, 추상화된 현실의 개념을 구체적인 파이썬 코드로 표현하기 위해, 빵틀과 빵의 관계 만들기 위해
 class Cat: #대문자가 관습
-    def __init__(self, name, color): #매소드(클래스의 함수), self는 인스턴스를 지칭함 이걸 통해 매소드 안의 내용을 구성할 수 있다.
+    def __init__(self, name, color): #매소드(클래스의 함수), self는 인스턴스를 지칭함(cat1, cat2 각각) 이걸 통해 매소드 안의 내용을 구성할 수 있다.
         self.name = name #인스턴스 변수 name 생성
         self.color = color #인스턴스 변수 color 생성
     def info(self):
