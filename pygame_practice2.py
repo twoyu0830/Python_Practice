6ad01310c1385d605c4b26889d882598944aa322
twoyu0830 <yygg0830@gmail.com>
2020-10-07 22:46:34 +0900

    Shooting Game

--- pygame_practice2.py
+++ pygame_practice2.py
@@ -3,22 +3,25 @@ import pygame
 pygame.init()
 
 # 2. 게임창 옵션 설정
-size = [400, 800]
+size = [400, 700]
 screen = pygame.display.set_mode(size)
 
-title = 'My Game'
+title = 'Shooting Game'
 pygame.display.set_caption(title)
 
 # 3. 게임 내 필요한 설정
 clock = pygame.time.Clock()
 white = (255, 255, 255)
-black = (0, 0, 0)
-k = 0
 
-
+ss = pygame.image.load('C:/Users/ygyg0/Google 드라이브/배움/python_practice/ss.png').convert_alpha()
+ss = pygame.transform.scale(ss, (50, 80))
+ss_sx, ss_sy = ss.get_size()
+ss_x = round(size[0] / 2 - ss_sx / 2)
+ss_y = size[1] - ss_sy - 10
 # 4. 메인 이벤트
 SB = 0
 while SB == 0:
+    
     # 4-1. FPS 설정
     clock.tick(60)
  
@@ -28,14 +31,11 @@ while SB == 0:
             SB = 1
  
     # 4-3. 입력, 시간에 따른 변화
-    k += 1
-    if k % 2 == 0:
-        color = white
-    else:
-        color = black
- 
+    
+
     # 4-4. 그리기
-    screen.fill(color)
+    screen.fill(white)
+    screen.blit(ss, (ss_x, ss_y))
  
     # 4-5 업데이트
     pygame.display.flip()
