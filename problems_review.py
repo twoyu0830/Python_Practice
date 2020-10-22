17f42ab8fa6a819442ac304237fcb2dd3e7a5e6e
twoyu0830 <yygg0830@gmail.com>
2020-10-22 20:59:41 +0900

    Problems Review

---
+++ problems_review.py
@@ -0,0 +1,100 @@
+#1. a = [1, 2, 5, 4, 3]를 순서대로 만들어보세요.
+a = [1, 2, 5, 4, 3]
+a = list(set(a))
+print(a)
+
+a = [1, 2, 5, 4, 3]
+a.sort()
+print(a)
+
+###############2. a= [1, 2, 5, 4, 3]를 a = [5, 4, 3, 2, 1]로 만들어보세요.
+ a= [1, 2, 5, 4, 3]
+ a.sort(reverse = True)
+ print(a)
+
+
+#3. a = ['h', 'e', 'l', 'l', 'o']를 a = 'hello'로 만들어보세요.
+a = ['h', 'e', 'l', 'l', 'o']
+a = ''.join(a)
+print(a)
+
+################4. '경기도 평택시 중앙로 176 101동 706호'를 빈 칸을 기준으로 분리시킨 후(리스트 안에서) 각각 출력하시오
+add = '경기도 평택시 중앙로 176 101동 706호'
+add1 = add.split(' ')
+for i in range(len(add1)):
+    print(add1[i])
+
+#5. a = (1, 2, 3)에 4를 추가하시오.
+a = (1, 2, 3)
+a = a + (4,)
+print(a)
+
+#######################6. dic = {'a': 2, 'b': 3, 'c' : 4}를 이용해 del과 pop의 차이점을 보이시오.
+dic = {'a': 2, 'b': 3, 'c' : 4}
+del dic['a']
+print(dic)
+
+dic = {'a': 2, 'b': 3, 'c' : 4}
+dic1 = dic.pop('a')
+print(dic1)
+print(dic)
+
+
+
+#7. a = [1, 1, 3, 3, 2, 2, 3, 1, 2, 3, 2, 1]을 a = [1, 2, 3]으로 바꾸시오.
+a = [1, 1, 3, 3, 2, 2, 3, 1, 2, 3, 2, 1]
+a = list(set(a))
+print(a)
+
+#8. [1, 4, 9, 16, 25]를 출력하시오.
+squ_list = []
+for i in range(1, 6):
+    num = i ** 2
+    squ_list.append(num)
+print(squ_list)
+
+############9.이윤규의 총점과 평균을 구하시오.
+score = []
+score.append({'이름': '이윤규', '국어': 98, '영어': 100, '수학': 100})
+score.append({'이름': '김도희', '국어': 93, '영어': 100, '수학': 95})
+score.append({'이름': '이정환', '국어': 90, '영어': 100, '수학': 100})
+score.append({'이름': '이윤지', '국어': 100, '영어': 100, '수학': 99})
+
+tot = score[0]['국어'] + score[0]['영어'] + score[0]['수학']
+avg = tot / 3
+print(tot, avg)
+
+sco = []
+sco.append(score[0]['국어'])
+sco.append(score[0]['수학'])
+sco.append(score[0]['영어'])
+print(sum(sco), sum(sco) / len(sco))
+
+
+
+#10. 이 사람들의 국어 점수의 총합을 구하시오.
+score = []
+score.append({'이름': '이윤규', '국어': 98, '영어': 100, '수학': 100})
+score.append({'이름': '김도희', '국어': 93, '영어': 100, '수학': 95})
+score.append({'이름': '이정환', '국어': 90, '영어': 100, '수학': 100})
+score.append({'이름': '이윤지', '국어': 100, '영어': 100, '수학': 99})
+
+tot_kor = []
+for i in range(len(score)):
+    tot_kor.append(score[i]['국어'])
+print(sum(tot_kor))
+
+tot_kor = 0
+for i in range(len(score)):
+    tot_kor += score[i]['국어']
+print(tot_kor)
+
+tot_kor = 0
+for i in score:
+    tot_kor += i['국어']
+print(tot_kor)
+
+
+
+
+
